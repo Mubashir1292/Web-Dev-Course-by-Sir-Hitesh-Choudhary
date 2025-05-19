b4969ca7fa100ad770c96c77c1a31e196efec0f9
Mubashir1292 <mubashirliaqat72@gmail.com>
2025-05-19 14:04:41 +0500

    add the winston and morgan packages..

--- Backend/controller/book.controller.js
+++ Backend/controller/book.controller.js
@@ -1,24 +1,43 @@
-import {books} from '../books.js'
-export const getAllBooks=async(req,res)=>{
-    try{
-        const allBooks=await books;
-        if(!allBooks){
-            return res.status(404).json({message:"Books not founded"});
-        } 
+import { books } from '../books.js'
+export const getAllBooks = async (req, res) => {
+    try {
+        const allBooks = await books;
+        if (!allBooks) {
+            return res.status(404).json({ message: "Books not founded" });
+        }
         res.status(200).json(allBooks);
-    }catch(error){
-        res.status(500).json({message:"Error while fetching the books"});
+    } catch (error) {
+        res.status(500).json({ message: "Error while fetching the books" });
     }
-} 
-export const getBookById=(req,res)=>{
-    try{
-        const id=req.params.id;
-        const book=books.find(item=>item.id===id);
-        if(!book){
-            return res.status(404).json({message:"Book not founded"});
+}
+export const getBookById = (req, res) => {
+    try {
+        const id = req.params.id;
+        const book = books.find(item => item.id === id);
+        if (!book) {
+            return res.status(404).json({ message: "Book not founded" });
         }
         res.status(200).json(book);
-    }catch(error){
-        res.status(500).json({message:"Error while searching book"})
+    } catch (error) {
+        res.status(500).json({ message: "Error while searching book" })
+    }
+}
+export const createBook = (req, res) => {
+    try {
+        const { title, author, genre, year } = req.body;
+        if (!title || !author || !genre || !year) {
+            return res.status(400).json({ message: "Some params not passed" });
+        }
+        const newBook={
+            id:books.length+1,
+            title,
+            author,
+            genre,
+            year
+        };
+        books.push(newBook);
+        res.status(201).json({message:"Book Created.."})
+    } catch (error) {
+        res.status(500).json({ message: "Error while creating book.." });
     }
 }
--- Backend/routes/book.route.js
+++ Backend/routes/book.route.js
@@ -1,7 +1,8 @@
 import express from "express";
-import { getAllBooks, getBookById } from "../controller/book.controller.js";
+import { createBook, getAllBooks, getBookById } from "../controller/book.controller.js";
 const app=express();
 const router=app.router;
 router.get("/api/books",getAllBooks);
 router.get("/api/book/:id",getBookById);
+router.post("/api/book",createBook);
 export default router;
