ba45c3c7cca09a497a97b6eaf7964c5954c024a5
Mubashir1292 <mubashirliaqat72@gmail.com>
2025-06-11 18:17:17 +0500

    add the local User related things..

--- BackendV2/src/controllers/user.controller.js
+++ BackendV2/src/controllers/user.controller.js
@@ -1,5 +1,6 @@
 import asyncHandler from '../utils/AsyncHandler.js';
 import ApiError from '../utils/ApiError.js';
+import User from '../models/user.models.js';
 const registerUser = asyncHandler(async (req, res) => {
     // getting all the data from the user.routes.js...
     const { fullName, username, email, password } = req.body;
@@ -11,5 +12,21 @@ const registerUser = asyncHandler(async (req, res) => {
     ) {
         throw new ApiError(400, "All fields are required")
     }
+    const existedUser = await User.findOne({
+        $or:[{username},{email}]
+    })
+    if(existedUser){
+        throw new ApiError(400,"The User with same username and email exists..")
+    }
+    //? checking the avatar file's local path exists or not..
+    const avatarLocalPath = req.files?.avatar[0]?.path;
+    const coverImageLocalPath = req.files?.coverImage[0]?.path;
+    if(avatarFile){
+        
+    }
+
+
+
+    // till now we just add the some relevant things into the file.. 
 });
 export default registerUser;
