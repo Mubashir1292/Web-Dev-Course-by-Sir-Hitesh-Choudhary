5946268fe8f60c3783b728a2b5cc38205f5160d6
Mubashir1292 <mubashirliaqat72@gmail.com>
2025-06-11 18:29:24 +0500

    add the cloudinary setup to upload the local file path

--- BackendV2/src/controllers/user.controller.js
+++ BackendV2/src/controllers/user.controller.js
@@ -1,6 +1,7 @@
 import asyncHandler from '../utils/AsyncHandler.js';
 import ApiError from '../utils/ApiError.js';
 import User from '../models/user.models.js';
+import { uploadOnCloudinary } from '../utils/cloudinary.js';
 const registerUser = asyncHandler(async (req, res) => {
     // getting all the data from the user.routes.js...
     const { fullName, username, email, password } = req.body;
@@ -21,10 +22,12 @@ const registerUser = asyncHandler(async (req, res) => {
     //? checking the avatar file's local path exists or not..
     const avatarLocalPath = req.files?.avatar[0]?.path;
     const coverImageLocalPath = req.files?.coverImage[0]?.path;
-    if(avatarFile){
-        
+    if(!avatarFile || !coverImageLocalPath){
+        throw new ApiError(200,"The files local path is missing..")
     }
-
+    // uploading this localfile path to the cloudinary for the sake of getting it back when we need it...
+    await uploadOnCloudinary(avatarLocalPath);
+    
 
 
     // till now we just add the some relevant things into the file.. 
